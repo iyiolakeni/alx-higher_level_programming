68c2fe2b4731fb5dd1ab2d570af3f898a79c6c6a
iyiolakeni <iyiolakeni@gmail.com>
2023-06-12 03:19:06 -0700

    Divisinle added bracket

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    myList = []
-
-    for x in range(len(my_list)):
-        if (my_list[x] % 2) == 0:
-            myList.append(True)
-        else:
-            myList.append(False)
-        return myList
+    new_list = [True if (i % 2) == 0 else False for i in my_list]
+    return (new_list)
