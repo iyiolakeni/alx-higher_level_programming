67e96a6e6b2f5027b5c684a2f1c24955299d9904
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-12 02:32:15 +0100

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 function factorial(n) {
-  if (isNaN(n) || n < 0) {
+  if (isNaN(n)) {
     return (1);
   }
-  if (n === 0 || n === 1) {
+  if (n === 0) {
     return (1);
   } else {
     return (n * factorial(n - 1));
