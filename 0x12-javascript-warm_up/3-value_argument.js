e7dfd0bf10c8c2e3b1e4f22778ea37a5850fdfcd
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-12 02:10:01 +0100

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args.length === 0) {
+const args = process.argv[2];
+if (typeof firstArg === 'undefined') {
   console.log('No argument');
 } else {
-  console.log(args[0]);
+  console.log(firstArg);
 }
