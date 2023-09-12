6141a1301f50a4a5a051286893db47dfb1bd555a
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-12 02:43:27 +0100

    Update 101-call_me_moby.js

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 
-function callMeMoby(x, theFunction) {
-  for (let i = 0; i < x; i++) {
-    theFunction();
-  }
-}
-
-module.exports.callMeMoby = callMeMoby;
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) theFunction();
+};
