58442255236c548a4b930fed5205df277aa165c3
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-12 02:22:16 +0100

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-const x = Math.floor(Number(process.argv[2]));
-if (isNaN(x)) {
-  console.log('Missing number of occurrences');
-} else {
-  for (let i = 0; i < x; i++) {
+
+const num = parseInt(process.argv[2]);
+
+if (!isNaN(num) && num > 0) {
+  for (let i = 0; i < num; i++) {
     console.log('C is fun');
   }
+} else {
+  console.log('Missing number of occurrences');
 }
