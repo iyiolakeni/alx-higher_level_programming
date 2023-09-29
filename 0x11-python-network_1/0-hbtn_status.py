208d2473398e5f0dbec5b9adea5dd937756fad14
iyiolakeni <iyiolakeni@gmail.com>
2023-09-29 12:36:25 -0700

    Fetch

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status."""
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+with urlib.request.urlopen(url) as response:
+    body = response.read()
+    utf8_content = body.decode('utf-8')
+    print("Body response:")
+    print("     - type:", type(body))
+    print("     - content:", body)
+    print("     - utf8 content:", utf8_content)
